afc303adf067bc487c87df75fa063b87a27d10f1
siu2388 <111097199+siu2388@users.noreply.github.com>
2023-01-26 16:23:07 +0900

    deploy연습
    1

---
+++ database.py
@@ -0,0 +1,40 @@
+import os
+
+from deta import Deta  # pip install deta
+from dotenv import load_dotenv  # pip install python-dotenv
+
+
+# load the environment variables
+load_dotenv(".env")
+DETA_KEY = os.getenv('DETA_KEY')
+
+# initailize with a project key
+deta = Deta(DETA_KEY)
+
+# this is how to create/connect a database
+db = deta.Base('monthly_report')
+
+# 데이터베이스에 특정 기간의 값을 삽입
+
+
+def insert_period(period, incomes, expenses, comment):
+    """Returns the report on a successful creation, otherwise raises an error"""
+    return db.put({"key": period, "incomes": incomes, "expenses": expenses, "comment": comment})
+
+# selectbox채우기 위해 모든 기간 가져오기
+
+
+def fetch_all_periods():
+    """Returns a dict of all periods"""
+    res = db.fetch()
+    return res.items
+
+# 특정기간 모든 값 가져와서 데이터 플롯
+
+
+def get_period(period):
+    """If not found, the function will return None"""
+    return db.get(period)
+
+
+#deta.sh siu2388 Govlchfhd0! Dashboard 
